41dd5542405c56597de7400d5126dcf2d7ffc7f9
Bula <umbetalyevv@gmail.com>
2023-01-06 17:47:14 +0600

    Initial commit

---
+++ NetflixClone/MainTabBarViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainTabBarViewController.swift
+//  NetflixClone
+//
+//  Created by Bula on 1/6/23.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemYellow
+    }
+
+
+}
+
