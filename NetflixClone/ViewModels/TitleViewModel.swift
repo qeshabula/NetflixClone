52f17991951639d4456f530be163492fe5279d94
Bula <umbetalyevv@gmail.com>
2023-01-08 20:18:37 +0600

    Configuration UpcomingViewController

---
+++ NetflixClone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  NetflixClone
+//
+//  Created by Bula on 1/8/23.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
